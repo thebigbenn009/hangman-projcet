2279d2e2258bf584172b13457c68d269670215f1
Benedict <aliubenedict@gmail.com>
2024-10-03 23:02:02 +0100

    working on the front-end-1

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91d71246a4ce37be"; }
+/******/ 		__webpack_require__.h = function() { return "2a14a18c15b0cca4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
